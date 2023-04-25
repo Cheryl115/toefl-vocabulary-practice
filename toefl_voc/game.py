f7753cb379600e0d847ec006a2d3726fd0404471
方琬淳 <cheryl@fangwanchundeiMac.local>
2023-04-25 18:41:58 +0800

    2023-04-25

--- toefl_voc/game.py
+++ toefl_voc/game.py
@@ -24,17 +24,20 @@ with open('voc.csv', 'r',encoding="UTF-8") as csvfile:
 csvfile.close
 
 var = tk.StringVar()    # 这时文字变量储存器
-vocabulary = tk.Label(window, textvariable=var, font=('Arial', 12)) # 使用 textvariable 替换 text, 因为这个可以变化
+vocabulary = tk.Label(window, textvariable=var, font=('Noto Sans TC', 14)) # 使用 textvariable 替换 text, 因为这个可以变化
 vocabulary.pack()
 
 var1 = tk.StringVar()    # 这时文字变量储存器
-pos = tk.Label(window, textvariable=var1, font=('Arial', 12)) # 使用 textvariable 替换 text, 因为这个可以变化
+pos = tk.Label(window, textvariable=var1, font=('Noto Sans TC', 14)) # 使用 textvariable 替换 text, 因为这个可以变化
 pos.pack()
 
 
 var2 = tk.StringVar()    # 这时文字变量储存器
-meaning = tk.Label(window, textvariable=var2, font=('Arial', 12)) # 使用 textvariable 替换 text, 因为这个可以变化
+meaning = tk.Label(window, textvariable=var2, font=('Noto Sans TC', 14)) # 使用 textvariable 替换 text, 因为这个可以变化
 meaning.pack()
+
+text = tk.StringVar()
+text.set('Start')
 
 
 btn1 = False
@@ -47,10 +50,13 @@ def btn1_click():
         while(flag==True):
             voc = random.choice(vocs)
             try:
-                var.set(voc[0])
-                var1.set(voc[1])
-                var2.set(voc[2])
-                flag = False
+                if voc[0]!='None' and voc[1]!='None' and voc[2]!='None':
+                    var.set(voc[0])
+                    var1.set(voc[1])
+                    var2.set(voc[2])
+                    text.set('Next')
+                    flag = False
+                    
             except Exception:
                 flag = True
     else:
@@ -61,7 +67,7 @@ def btn1_click():
         
 
 # 第5步，在視窗介面設定放置Button按鍵
-b1 = tk.Button(window, text='Next', font=('Arial', 12), width=10, height=1, command=btn1_click)
+b1 = tk.Button(window, textvariable=text , font=('Noto Sans TC', 14), width=10, height=1, command=btn1_click)
 b1.pack()
 
 canvas = tk.Canvas(window, height=500, width=500)
